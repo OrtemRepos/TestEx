b87eb91ae31d7d92356a92e8cf5dd710d718e63e
Artem Sadykov <ortem622@gmail.com>
2024-09-16 21:29:54 +0500

    modify pydantic model

--- src/schema.py
+++ src/schema.py
@@ -1,10 +1,32 @@
+from datetime import datetime
 from uuid import UUID
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict, EmailStr
 
 
-class User(BaseModel):
-    id: UUID
-    username: str
-    email: str
-    full_name: str
-    disabled: bool
+class UserCreate(BaseModel):
+    first_name: str
+    last_name: str
+    email: EmailStr
+    password: str
+    is_superuser: bool = False
+    is_active: bool = True
+
+
+class UserRead(BaseModel):
+    user_id: UUID
+    email: EmailStr
+    first_name: str
+    last_name: str
+    is_superuser: bool
+    is_active: bool
+    created_at: datetime
+    update_at: datetime | None = None
+
+
+class UserUpdate(BaseModel):
+    first_name: str | None = None
+    last_name: str | None = None
+    email: EmailStr | None = None
+    password: str 
+    is_superuser: bool = False
+    is_active: bool = True
