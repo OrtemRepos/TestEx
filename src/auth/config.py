5784033f5cf994bc3f427cdbcd76c4f2fa61b2b6
Artem Sadykov <ortem622@gmail.com>
2024-09-16 21:31:38 +0500

    add new config data

--- src/auth/config.py
+++ src/auth/config.py
@@ -7,6 +7,8 @@ load_dotenv("src/auth/.env")
 
 class Config:
     JWT_SECRET = os.environ.get("JWT_SECRET")
+    token_life_time = 3600
+    refresh_token_life_time = 3600 * 24 * 30
 
 
 config = Config()
